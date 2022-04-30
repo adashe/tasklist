418035792e58332b42b453eaf9b853df0ae1efe8
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-04-30 22:29:44 +0000

    Added User class to data model

---
+++ model.py
@@ -0,0 +1,19 @@
+"""Models for tasklist app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.model):
+    """A user."""
+
+    __tablename___ = "users"
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} username={self.username}>'
