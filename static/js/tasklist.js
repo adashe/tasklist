15c719f9f7807c96f946d32c81e2dc6c3cfff757
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-07-05 18:10:00 +0000

    Added closeable list items

--- static/js/tasklist.js
+++ static/js/tasklist.js
@@ -1,12 +1,10 @@
+// Get all elements with class="close"
+var closebtns = document.getElementsByClassName("close");
+var i;
 
-// const completeButton = document.querySelector('#complete-button');
-
-// function toggleComplete() {
-//   if (completeButton.innerText === 'Incomplete') {
-//     completeButton.innerText = 'Complete';
-//   } else if (completeButton.innerText === 'Complete') {
-//     completeButton.innerText = 'Incomplete';
-//   }
-// }
-
-// completeButton.addEventListener('click', toggleComplete);
+// Loop through the elements, and hide the parent, when clicked on
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
